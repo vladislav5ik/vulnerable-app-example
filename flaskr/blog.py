62f3fd3ab71cf9bea73d66913ce6981d9b023541
vladislav5ik <33522722+vladislav5ik@users.noreply.github.com>
2023-04-18 20:51:09 +0300

    added Broken Access Control vulnerability Any user can update any posts

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -87,7 +87,16 @@ def create():
 @login_required
 def update(id):
     """Update a post if the current user is the author."""
-    post = get_post(id)
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if request.method == "POST":
         title = request.form["title"]
